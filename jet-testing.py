ce11c95d96c1038d8c22700ea199554d80086bd2
Daniel <danielshrives@gmail.com>
2019-10-05 18:51:42 -0400

    now in amps, needed common ground...

--- jet-testing.py
+++ jet-testing.py
@@ -48,8 +48,8 @@ def publish_temp_status():
 
 def publish_adc_status():
 
-	#jet1_amps = ((chan.voltage - 2.47) / 0.013) * 100
-	#jet2_amps = ((chan2.voltage - 2.47) / 0.013) * 100
+	jet1_amps = ((chan.voltage - 2.47) / 0.013) * 100
+	jet2_amps = ((chan2.voltage - 2.47) / 0.013) * 100
 
 	jet1_amps = chan.voltage
 	jet2_amps = chan2.voltage
