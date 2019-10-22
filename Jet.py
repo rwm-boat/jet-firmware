3f01bbea444e55ad928f5107999c1044291f6a9e
brentrolfes <brentrolfes20@gmail.com>
2019-10-22 18:35:20 -0400

    creation of Jet class

---
+++ Jet.py
@@ -0,0 +1,19 @@
+from adafruit_servokit import ServoKit
+import time
+kit = ServoKit(channels=16)
+
+class Jet:
+
+    def __init__(self, port_jet):
+        self.port_jet = port_jet
+        if port_jet == False: #Jet1 Starboard Jet
+            ESC1 = kit.servo[4]
+            RB1 = kit.servo[5]
+            DIR1 = kit.servo[6]
+        if port_jet == True: #Jet2 Port Jet
+            ESC2 = kit.servo[0]
+            RB2 = kit.servo[1]
+            DIR2 = kit.servo[2]
+
+Jet1 = Jet(False)
+Jet2 = Jet(True)
