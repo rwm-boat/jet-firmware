cd58f0ef2580251977b9f13f0d69941894834e4f
brentrolfes <brentrolfes20@gmail.com>
2019-10-23 15:41:45 -0400

    trying director sweep

--- test_jet.py
+++ test_jet.py
@@ -7,5 +7,13 @@ def main():
     Jet1.zero()
     Jet2.zero()
 
+    while(True):
+        for x in range(-25,25):
+            Jet1.dir_rq(x)
+            Jet2.dir_rq(x)
+        for y in range(25,-25):
+            Jet1.dir_rq(y)
+            Jet2.dir_rq(y)
+
 if __name__ == "__main__":
     main()
