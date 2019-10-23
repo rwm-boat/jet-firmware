32d7d5848bef4f532aad8dd19a90e2605d9ee85c
brentrolfes <brentrolfes20@gmail.com>
2019-10-23 16:43:04 -0400

    dasd

--- test_jet.py
+++ test_jet.py
@@ -8,18 +8,20 @@ def main():
     Jet1.zero()
     Jet2.zero()
 
-    Jet1.startup()
-    Jet2.startup()
-
-    # while(True):
-    #     for x in range(-25,25,1):
-    #         Jet1.dir_rq(x)
-    #         Jet2.dir_rq(x)
-    #         time.sleep(0.1)
-    #     for y in range(25,-25,-1):
-    #         Jet1.dir_rq(y)
-    #         Jet2.dir_rq(y)
-    #         time.sleep(0.1)
+    while(True):
+        for x in range(-25,25,1):
+            Jet1.dir_rq(x)
+            Jet2.dir_rq(x)
+            time.sleep(0.1)
+        for y in range(25,-25,-1):
+            Jet1.dir_rq(y)
+            Jet2.dir_rq(y)
+            time.sleep(0.1)
+        Jet1.rb_rq('down')
+        Jet2.rb_rq('down')
+        time.sleep(1)
+        Jet1.rb_rq('up')
+        Jet2.rb_rq('up')
 
 if __name__ == "__main__":
     main()
