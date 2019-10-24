232632c149ba4ded1ba56eca97e944f36b8fafad
brentrolfes <brentrolfes20@gmail.com>
2019-10-23 20:55:55 -0400

    created jet-ctrl

---
+++ jet-ctrl.py
@@ -0,0 +1,28 @@
+from jet import Jet
+import jet-logging
+import time
+
+def main():
+    Jet1 = Jet(False)
+    Jet2 = Jet(True)
+
+    Jet1.zero()
+    Jet2.zero()
+
+    while(True):
+        for x in range(-25,25,1):
+            Jet1.dir_rq(x)
+            Jet2.dir_rq(x)
+            time.sleep(0.1)
+        for y in range(25,-25,-1):
+            Jet1.dir_rq(y)
+            Jet2.dir_rq(y)
+            time.sleep(0.1)
+        Jet1.rb_rq('down')
+        Jet2.rb_rq('down')
+        time.sleep(1)
+        Jet1.rb_rq('up')
+        Jet2.rb_rq('up')
+
+if __name__ == "__main__":
+    main()
