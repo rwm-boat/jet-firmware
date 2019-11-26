75363445512a1cf99898526aee99d5e45633a83f
danielshrives <danielshrives@gmail.com>
2019-11-25 23:27:27 -0500

    T

--- JTR_plotting.py
+++ JTR_plotting.py
@@ -9,20 +9,25 @@ path = easygui.fileopenbox()
 
 jet_current = []
 thrust = []
+efficiency = []
+
 
 def load_log():
     global jet_current
     global thrust
+    global efficiency
 
     with open(path, "r") as log_file:
          for line in log_file.readlines():
             obj = json.loads(line)
             jet_current.append(obj["current"])
-            thrust.append(obj["thrust"]*9.8)
+            thrust.append(obj["thrust"])
+            efficiency.append(obj["thrust"]/obj["current"])
 def plot():
     fig, ax1 = plt.subplots()
 
-    ax1.plot(jet_current, label="Jet Current (Amps)", color = 'g')
+    ax1.plot(jet_current, label="Jet Current", color = 'g')
+    
 
     ax2 = ax1.twinx() #second y axis
     ax2.plot(thrust, label="Thrust", color="r")
@@ -32,8 +37,17 @@ def plot():
     ax1.set_xlabel("Time (deciseconds)")
     ax1.set_ylabel("Jet Current Draw (Amps)")
     ax1.legend(loc = 'lower right')
-    ax2.set_ylabel("Thrust Newtons")
+    ax2.set_ylabel("Thrust (grams-force)")
     ax1.grid()
+    #plt.figure()
+    fig, ax3 = plt.subplots()
+    ax3.plot(efficiency, label="Jet Efficiency", color = 'b')
+    plt.title("Efficiency (grams-force/Amp)")
+    ax3.set_xlabel("Time (deciseconds)")
+    ax3.set_ylabel("Efficiency (grams-force/Amp)")
+    ax3.legend(loc = 'lower right')
+    ax3.grid()
+
     plt.show()
 
 load_log()
