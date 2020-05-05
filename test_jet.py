1508da5033c981825350be2d97f1c9e1ab2548eb
brentrolfes <brentrolfes20@gmail.com>
2020-05-05 17:18:50 -0400

    b

--- test_jet.py
+++ test_jet.py
@@ -9,20 +9,20 @@ def main():
     Jet1.zero()
     Jet2.zero()
 
-    while(True):
-        for x in range(-25,25,1):
-            Jet1.dir_rq(x)
-            Jet2.dir_rq(x)
-            time.sleep(0.1)
-        for y in range(25,-25,-1):
-            Jet1.dir_rq(y)
-            Jet2.dir_rq(y)
-            time.sleep(0.1)
-        Jet1.rb_rq('down')
-        Jet2.rb_rq('down')
-        time.sleep(1)
-        Jet1.rb_rq('up')
-        Jet2.rb_rq('up')
+    # while(True):
+    #     for x in range(-25,25,1):
+    #         Jet1.dir_rq(x)
+    #         Jet2.dir_rq(x)
+    #         time.sleep(0.1)
+    #     for y in range(25,-25,-1):
+    #         Jet1.dir_rq(y)
+    #         Jet2.dir_rq(y)
+    #         time.sleep(0.1)
+    #     Jet1.rb_rq('down')
+    #     Jet2.rb_rq('down')
+    #     time.sleep(1)
+    #     Jet1.rb_rq('up')
+    #     Jet2.rb_rq('up')
 
 if __name__ == "__main__":
     main()
